78d00d566e5cb03fbf76451f7904d97d9c3f3d13
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-12-17 12:22:47 -0500

    huh

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,12 @@ async function getData(URL) {
   try {
     const response = await fetch(URL);
     const pokemon = await response.json();
+    pullPokemon(pokemon);
   } catch (error) {}
 }
 //pull a random pokemon from api, have input bar for name of pokemon, only accept valid name of pokemon, add inputted pokemon to an array, make sure user doesn't repeat their guesses, have a separate list to make sure the same pokemon isnt pulled, when user inputs a guess compare the categories of the guess with the pulled pokemon using api data, show a vicotry screen if user gets it right, show a defeat screen if they dont get it in 6 guesses
+function pullPokemon(array) {
+  let arrayLength = array.length;
+  let randomNumber = Math.floor(Math.random() * (arrayLength + 1));
+  console.log(randomNumber);
+}
