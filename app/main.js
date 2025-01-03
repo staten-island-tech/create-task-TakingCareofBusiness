1f625fb4bb735b21ef2ece0f6f13c19cf9e88ca2
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-01-03 12:12:56 -0500

    Pulled pork

--- app/main.js
+++ app/main.js
@@ -2,9 +2,11 @@
 const URL = "https://pokeapi.co/api/v2/pokemon";
 async function getData(URL) {
   try {
-    const response = await fetch(URL);
-    const pokemon = await response.json();
-    pullPokemon(pokemon);
+    const originalResponse = await fetch(URL);
+    const pokemon = await originalResponse.json();
+    let usableAPI = pullPokemon(basicPokemon);
+    const response = await fetch(usableAPI);
+    const pulledPokemon = await response.json();
   } catch (error) {}
 }
 //pull a random pokemon from api, have input bar for name of pokemon, only accept valid name of pokemon, add inputted pokemon to an array, make sure user doesn't repeat their guesses, have a separate list to make sure the same pokemon isnt pulled, when user inputs a guess compare the categories of the guess with the pulled pokemon using api data, show a vicotry screen if user gets it right, show a defeat screen if they dont get it in 6 guesses
@@ -12,12 +14,6 @@ function pullPokemon(pokemon) {
   let max = pokemon.count;
   let randomNumber = Math.floor(Math.random() * (max + 1));
   console.log(randomNumber);
-  let skibidi_number = randomNumber - (randomNumber % 20);
-  return `https://pokeapi.co/api/v2/pokemon?offset=${skibidi_number}&limit=20`;
-  /*
-  let pulledPokemon = Object.values(pokemon).filter(
-    (mon) => mon.results === randomNumber
-  );
-  */
+  return `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
 }
 getData(URL);
