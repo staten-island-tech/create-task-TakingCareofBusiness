5352d25c62b2aae91565fe1d2922d804f66a09fd
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-01-09 12:30:00 -0500

    Kyle is touching me in my private areas

--- app/main.js
+++ app/main.js
@@ -2,19 +2,22 @@ import "./style.css";
 const DOMSelectors = {
   form: document.querySelector(".userInput"),
   button: document.querySelector(".submitButton"),
+  textBar: document.querySelector(".inputForm"),
 };
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
 let guessed = [];
 let pulled = [];
-async function getData() {
+async function getData(URL) {
   try {
     let usableAPI = pullPokemon();
     let response1 = await fetch(usableAPI[0]);
     let pulledPokemonInfo1 = await response1.json();
     let response2 = await fetch(usableAPI[1]);
     let pulledPokemonInfo2 = await response2.json();
-    console.log(pulledPokemonInfo1);
-    console.log(pulledPokemonInfo2);
+    let responseFull = await fetch(URL);
+    let allPokemon = await responseFull.json();
+    let allNames = Object.values(allPokemon.results);
+    validGuess(allNames);
   } catch (error) {}
 }
 //pull a random pokemon from api, have input bar for name of pokemon, only accept valid name of pokemon, add inputted pokemon to an array, make sure user doesn't repeat their guesses, have a separate list to make sure the same pokemon isnt pulled, when user inputs a guess compare the categories of the guess with the pulled pokemon using api data, show a vicotry screen if user gets it right, show a defeat screen if they dont get it in 6 guesses
@@ -27,12 +30,27 @@ function pullPokemon() {
   ];
   return linkList;
 }
-function validGuess() {
-  DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();}
-  let input = event.target.va;ue
+function validGuess(allNames) {
+  console.log("skibidi");
+  DOMSelectors.button.addEventListener("click", function (event) {
+    console.log("Ou");
+    event.preventDefault();
+    let pokemonStatus = false;
+    let input = DOMSelectors.textBar.value;
+    allNames.forEach((title) => {
+      if (input === title.name) {
+        pokemonStatus = true;
+      }
+    });
+    if (pokemonStatus != true) {
+      DOMSelectors.textBar.insertAdjacentHTML(
+        "afterend",
+        "<p>Please input a real Pokemon name</p>"
+      );
+    }
+  });
 }
 function hintGenerator(info1, info2) {}
-getData();
+getData(URL);
 // blue is #3468a7
 //yellow is #fbcb0f
