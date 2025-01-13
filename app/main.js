621d376b90907e0299a5aa9ac15e1f701c32bc97
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-01-12 19:04:35 -0500

    Big progress

--- app/main.js
+++ app/main.js
@@ -4,6 +4,11 @@ const DOMSelectors = {
   button: document.querySelector(".submitButton"),
   textBar: document.querySelector(".inputForm"),
   errorText: document.querySelector(".errorText"),
+  hint1: document.querySelector(".hint1"),
+  hint2: document.querySelector(".hint2"),
+  hint3: document.querySelector(".hint3"),
+  hint4: document.querySelector(".hint4"),
+  hint5: document.querySelector(".hint5"),
 };
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
 let guessed = [];
@@ -19,7 +24,7 @@ async function getData(URL) {
     let responseFull = await fetch(URL);
     let allPokemon = await responseFull.json();
     let allNames = Object.values(allPokemon.results);
-    validGuess(allNames);
+    validGuess(allNames, pulledPokemonInfo1, pulledPokemonInfo2);
   } catch (error) {}
 }
 //pull a random pokemon from api, have input bar for name of pokemon, only accept valid name of pokemon, add inputted pokemon to an array, make sure user doesn't repeat their guesses, have a separate list to make sure the same pokemon isnt pulled, when user inputs a guess compare the categories of the guess with the pulled pokemon using api data, show a vicotry screen if user gets it right, show a defeat screen if they dont get it in 6 guesses
@@ -32,19 +37,19 @@ function pullPokemon() {
   ];
   return linkList;
 }
-function validGuess(allNames) {
+function validGuess(allNames, info1, info2) {
+  console.log(info1.name);
   DOMSelectors.button.addEventListener("click", function (event) {
     event.preventDefault();
     let pokemonStatus = false;
     let inputted = DOMSelectors.textBar.value;
-    console.log(inputted);
     let input = inputted.toLowerCase();
     allNames.forEach((title) => {
       if (input === title.name) {
         pokemonStatus = true;
       }
     });
-    if (!pokemonStatus) {
+    if (!pokemonStatus || guessed.includes(input)) {
       DOMSelectors.textBar.value = "";
       if (errorMessage === 0) {
         DOMSelectors.errorText.insertAdjacentHTML(
@@ -54,7 +59,7 @@ function validGuess(allNames) {
         errorMessage = 1;
       }
     }
-    if (pokemonStatus) {
+    if (pokemonStatus && !guessed.includes(input)) {
       DOMSelectors.textBar.value = "";
       if (errorMessage != 0) {
         let errors = document.querySelector(".error");
@@ -62,10 +67,59 @@ function validGuess(allNames) {
         errorMessage = 0;
       }
       guessed.push(input);
+      if (!(guessed.length === 0)) {
+        hintGenerator(info1, info2);
+      }
     }
   });
 }
-function hintGenerator(info1, info2) {}
+function hintGenerator(info1, info2) {
+  if (guessed.length === 1) {
+    DOMSelectors.hint1.insertAdjacentHTML(
+      "beforeend",
+      `<p class='hint'>This pokemon is from ${info1.generation.name}</p>`
+    );
+  }
+  if (guessed.length === 2) {
+    let evolvesFrom;
+    if (!info1.evolves_from_species) {
+      evolvesFrom = "This pokemon does not evolve from another pokemon!";
+    } else {
+      evolvesFrom = `This pokemon evolves from ${info1.evolves_fromspecies.name}`;
+    }
+    DOMSelectors.hint2.insertAdjacentHTML(
+      "beforeend",
+      `<p class='hint'>${evolvesFrom}</p>`
+    );
+  }
+  if (guessed.length === 3) {
+    DOMSelectors.hint3.insertAdjacentHTML(
+      "beforeend",
+      `<p class='hint'>This pokemon is ${info1.color.name}</p>`
+    );
+  }
+  if (guessed.length === 4) {
+    let typeName;
+    if (info2.types.length === 1) {
+      typeName = `This pokemon's type is ${info2.types[0].type.name}`;
+    }
+    if (info2.types.length === 2) {
+      typeName = `This pokemon's first type is ${info2.types[0].type.name} and their second type is ${info2.types[1].type.name}`;
+    }
+    DOMSelectors.hint4.insertAdjacentHTML(
+      "beforeend",
+      `<p class='hint'>${typeName}</p>`
+    );
+  }
+  if (guessed.length === 5) {
+    DOMSelectors.hint5.insertAdjacentHTML(
+      "beforeend",
+      `<p class='hint'>The first letter in this pokemon's name is ${info1.name.charAt(
+        0
+      )} </p>`
+    );
+  }
+}
 getData(URL);
 // blue is #3468a7
 //yellow is #fbcb0f
